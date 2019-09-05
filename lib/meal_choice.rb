c3de3a3451a4d9f2553d07fab8b7c72cc9b37d1a
David Caldwell <david@avvitt.com>
2019-09-05 18:55:29 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,15 +1,15 @@
-def meal_choice(food1="meat", food2, food3)
-  puts "A plate of #{food1} with #{food2} and #{food3}."
+def meal_choice(protein="meat", food2, food3)
+  puts "A plate of #{protein} with #{food2} and #{food3}."
 end
 
-food1 = "meat"
-meal_choice(food1,"broccoli","macaroni")
+protein = "meat"
+meal_choice(protein,"broccoli","macaroni")
 
 def meal_choice(food1="meat", food2, food3)
-  puts "A plate of #{food1} with #{food2} and #{food3}."
+  puts "A plate of #{protein} with #{food2} and #{food3}."
 end
 
-food1 = "meat"
+protein = "meat"
 meal_choice("tofu","broccoli","macaroni")
 
 puts "What a nutritious meal!" 
